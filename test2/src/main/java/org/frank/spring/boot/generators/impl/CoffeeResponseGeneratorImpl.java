b481d66593d4f5a899a741aba089085e43d10e6e
fengandzhy <fengandzhy@gmail.com>
2024-05-11 10:19:11 +1200

    CoffeeResponseGeneratorImpl

--- test2/src/main/java/org/frank/spring/boot/generators/impl/CoffeeResponseGeneratorImpl.java
+++ test2/src/main/java/org/frank/spring/boot/generators/impl/CoffeeResponseGeneratorImpl.java
@@ -2,11 +2,13 @@ package org.frank.spring.boot.generators.impl;
 
 import org.frank.spring.boot.generators.CoffeeResponseGenerator;
 import org.frank.spring.boot.models.CoffeeResponse;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CoffeeResponseGeneratorImpl implements CoffeeResponseGenerator {
 
     @Override
-    public CoffeeResponse brewCoffee() {
-        return null;
+    public CoffeeResponse brewCoffee(String message, String time) {
+        return new CoffeeResponse(message,time);
     }
 }

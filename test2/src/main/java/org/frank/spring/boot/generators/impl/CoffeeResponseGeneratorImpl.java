18d3d4ecd6dd5d69ba4dc31e3e49a659e078d11a
fengandzhy <fengandzhy@gmail.com>
2024-05-11 10:16:48 +1200

    CoffeeResponseGeneratorImpl

---
+++ test2/src/main/java/org/frank/spring/boot/generators/impl/CoffeeResponseGeneratorImpl.java
@@ -0,0 +1,12 @@
+package org.frank.spring.boot.generators.impl;
+
+import org.frank.spring.boot.generators.CoffeeResponseGenerator;
+import org.frank.spring.boot.models.CoffeeResponse;
+
+public class CoffeeResponseGeneratorImpl implements CoffeeResponseGenerator {
+
+    @Override
+    public CoffeeResponse brewCoffee() {
+        return null;
+    }
+}
